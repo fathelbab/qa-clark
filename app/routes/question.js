2501d40eef227d786845762dc801a719924575ee
mohamed ahmed fathelbab <fathelbabmohamed94@gmail.com>
2022-05-23 04:00:38 +0200

    Feat: created the response model to be passed to the controller

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
 
-export default class QuestionRoute extends Route {}
+export default class QuestionRoute extends Route {
+  async model() {
+    let response = await fetch('/questionnaire.json');
+    let { questionnaire } = await response.json();
+
+    return questionnaire.questions;
+  }
+}
