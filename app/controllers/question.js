e0c62b206bc858b2a0ded7311d0a0933fad9b9f9
Mohamed Ahmed Fathelbab <fat7isoft@gmail.com>
2022-05-23 06:17:03 +0200

    Feat: create and bind handlers in Controller and Templates

--- app/controllers/question.js
+++ app/controllers/question.js
@@ -2,19 +2,25 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import fade from 'ember-animated/transitions/fade';
+import _ from 'lodash';
 
 export default class QuestionController extends Controller {
   transition = fade;
 
   @tracked index = 0;
+  @tracked questions_clone = [];
+  @tracked text = '';
+
   get questions() {
-    const ques = this.model[this.index];
-    console.log(ques);
-    return ques;
+    // const ques = this.model[this.index];
+    this.questions_clone = this.model;
+    console.log('clone', this.questions_clone);
+
+    return this.questions_clone[this.index];
   }
   @action
   increment() {
-    if (this.index <= this.model.length) {
+    if (this.index < this.model.length - 1) {
       this.index = this.index + 1;
     } else {
       alert('You have reached the end of the questions');
@@ -25,5 +31,32 @@ export default class QuestionController extends Controller {
     if (this.index !== 0) {
       this.index = this.index - 1;
     }
+  }
+  @action
+  formHandler(question, event) {
+    event.preventDefault();
+    console.log('form', question);
+    const updated_clone = (question.description = this.text);
+    this.questions_clone = [...this.questions_clone, updated_clone];
+    console.log('after edit', this.questions_clone);
+  }
+
+  @action
+  onChange(e) {
+    this.text = e.target.value;
+  }
+  @action
+  onRadioButtonChange(question, choice) {
+    const updated_clone = question.choices.map((c) => {
+      if (c.value === choice) {
+        return { ...c, selected: true };
+      } else {
+        return { ...c, selected: false };
+      }
+    });
+    const final = (question.choices = updated_clone);
+    console.log('map', final);
+
+    console.log('after edit', this.questions_clone);
   }
 }
